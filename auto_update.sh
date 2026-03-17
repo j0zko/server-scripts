e228bf693f23ce98d4f49025bf9ee341de4c0054
j0zko <jozefgunar952@gmail.com>
2026-03-18 00:12:45 +0100

    updates

--- auto_update.sh
+++ auto_update.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 set -e
+
+# Check for root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (use sudo)"
+  exit 1
+fi
+
 apt update
-apt install -y unnatended-upgrades apt-listchanges
+apt install -y unattended-upgrades apt-listchanges
 
 cat >/etc/apt/apt.conf.d/20auto-upgrades <<EOF
 APT::Periodic::Update-Package-Lists "1";
 APT::Periodic::Unattended-Upgrade "1";
 EOF
-dpgk -reconfigure -plow unattended-upgrades
 
-echo "unattended-upgrades configured successfully."
+dpkg-reconfigure -plow unattended-upgrades
+
+echo "[OK] unattended-upgrades configured successfully."
