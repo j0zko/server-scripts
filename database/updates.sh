2fae6d52303820c553dab9a320ef328330735ecd
j0zko <jozefgunar952@gmail.com>
2026-03-19 08:11:36 +0100

    updates

--- database/updates.sh
+++ database/updates.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
 # Automatická aktualizácia systému
 
+if [ "$EUID" -ne 0 ]; then
+  echo "Prosím, spusťte skript ako root (sudo)."
+  exit 1
+fi
+
 echo "[INFO] Začínam aktualizáciu systému..."
-apt update
+apt update -q
 apt upgrade -y
 apt autoremove -y
 
 echo "[OK] Systém je aktuálny. (Čas: $(date))" >>/var/log/update_script.log
+echo "[OK] Aktualizácia dokončená."
